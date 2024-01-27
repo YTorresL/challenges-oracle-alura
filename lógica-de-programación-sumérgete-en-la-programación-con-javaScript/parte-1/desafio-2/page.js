c7f73414bdc12d29a841819db70b0f17e8c13547
Yalith Torres <yalithdev@gmail.com>
2024-01-26 23:35:06 -0400

    lógica-de-programación parte-1 | inicio estructura

---
+++ lógica-de-programación-sumérgete-en-la-programación-con-javaScript/parte-1/desafio-2/page.js
@@ -0,0 +1,19 @@
+// Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!". De lo contrario, muestra "¡Buena semana!".
+
+const FIN_DE_SEMANA = {
+  sabado: "Sábado",
+  domingo: "Domingo",
+};
+
+let preguntaUsuario = prompt("¿Que dia de la semana es?");
+
+if (
+  preguntaUsuario === FIN_DE_SEMANA.sabado ||
+  preguntaUsuario === FIN_DE_SEMANA.domingo
+) {
+  alert("¡Buen fin de semana!");
+} else {
+  alert("¡Buena semana!");
+}
+
+preguntaUsuario;
