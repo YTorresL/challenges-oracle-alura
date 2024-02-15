72731fc31f2c311f44f8793f93595437b88b779c
Yalith Torres <yalithdev@gmail.com>
2024-02-14 21:30:13 -0400

    Parte 1 desafio 1

--- archivos/lógica-de-programación-sumérgete-en-la-programación-con-javaScript/parte-1/desafio-1/page.js
+++ archivos/lógica-de-programación-sumérgete-en-la-programación-con-javaScript/parte-1/desafio-1/page.js
@@ -3,15 +3,64 @@
 const FIN_DE_SEMANA = {
   sabado: "Sábado",
   domingo: "Domingo",
+  lunes: "Lunes",
+  martes: "Martes",
+  miercoles: "Miércoles",
+  jueves: "Jueves",
+  viernes: "Viernes",
 };
 
-//let preguntaUsuario = prompt("¿Que dia de la semana es?");
+function Semana() {
+  const preguntaUsuario = document.getElementById("respuesta").value;
 
-if (
-  preguntaUsuario === FIN_DE_SEMANA.sabado ||
-  preguntaUsuario === FIN_DE_SEMANA.domingo
-) {
-  alert("¡Buen fin de semana!");
-} else {
-  alert("¡Buena semana!");
+  const convertirPreguntaUsuario = preguntaUsuario.toLowerCase();
+  const convertirSabado = FIN_DE_SEMANA.sabado.toLowerCase();
+  const convertirDomingo = FIN_DE_SEMANA.domingo.toLowerCase();
+  const convertirLunes = FIN_DE_SEMANA.lunes.toLowerCase();
+  const convertirMartes = FIN_DE_SEMANA.martes.toLowerCase();
+  const convertirMiercoles = FIN_DE_SEMANA.miercoles.toLowerCase();
+  const convertirJueves = FIN_DE_SEMANA.jueves.toLowerCase();
+  const convertirViernes = FIN_DE_SEMANA.viernes.toLowerCase();
+  const convertirPreguntaUsuariotildes = convertirPreguntaUsuario
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  const convertirSabadoTildes = convertirSabado
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  const convertirDomingoTildes = convertirDomingo
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  const convertirLunesTildes = convertirLunes
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  const convertirMartesTildes = convertirMartes
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  const convertirMiercolesTildes = convertirMiercoles
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  const convertirJuevesTildes = convertirJueves
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  const convertirViernesTildes = convertirViernes
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+
+  if (
+    convertirPreguntaUsuariotildes === convertirSabadoTildes ||
+    convertirPreguntaUsuariotildes === convertirDomingoTildes
+  ) {
+    document.getElementById("resultado").innerHTML = "¡Buen fin de semana!";
+  } else if (
+    convertirPreguntaUsuariotildes === convertirLunesTildes ||
+    convertirPreguntaUsuariotildes === convertirMartesTildes ||
+    convertirPreguntaUsuariotildes === convertirMiercolesTildes ||
+    convertirPreguntaUsuariotildes === convertirJuevesTildes ||
+    convertirPreguntaUsuariotildes === convertirViernesTildes
+  ) {
+    document.getElementById("resultado").innerHTML = "¡Buena semana!";
+  } else {
+    document.getElementById("resultado").innerHTML =
+      "¡No es un dia de la semana!";
+  }
 }
