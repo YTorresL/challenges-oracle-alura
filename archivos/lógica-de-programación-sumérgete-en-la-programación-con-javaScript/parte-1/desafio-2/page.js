7fa89b696d2f4b2ddbff79821fe3607382b92917
Yalith Torres <yalithdev@gmail.com>
2024-02-18 16:24:58 -0400

    Desafio 2 lógica-de-programación-sumérgete-en-la-programación-con-javaScript

--- archivos/lógica-de-programación-sumérgete-en-la-programación-con-javaScript/parte-1/desafio-2/page.js
+++ archivos/lógica-de-programación-sumérgete-en-la-programación-con-javaScript/parte-1/desafio-2/page.js
@@ -1,19 +1,16 @@
-// Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!". De lo contrario, muestra "¡Buena semana!".
+// Verifica si un número ingresado por el usuario es positivo o negativo. Muestra una alerta informativa.
 
-const FIN_DE_SEMANA = {
-  sabado: "Sábado",
-  domingo: "Domingo",
-};
+function Valor() {
+  const preguntaUsuario = document.getElementById('respuesta').value
+  const convertirPreguntaUsuario = Math.sign(preguntaUsuario)
 
-let preguntaUsuario = prompt("¿Que dia de la semana es?");
-
-if (
-  preguntaUsuario === FIN_DE_SEMANA.sabado ||
-  preguntaUsuario === FIN_DE_SEMANA.domingo
-) {
-  alert("¡Buen fin de semana!");
-} else {
-  alert("¡Buena semana!");
+  if (convertirPreguntaUsuario === 1) {
+    document.getElementById('resultado').innerHTML = '¡El número es positivo!'
+  } else if (convertirPreguntaUsuario === -1) {
+    document.getElementById('resultado').innerHTML = '¡El número es negativo!'
+  } else if (convertirPreguntaUsuario === 0) {
+    document.getElementById('resultado').innerHTML = '¡El número es 0!'
+  } else {
+    document.getElementById('resultado').innerHTML = '¡No es un numero!'
+  }
 }
-
-preguntaUsuario;
